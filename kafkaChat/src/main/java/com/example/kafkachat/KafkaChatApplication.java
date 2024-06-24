ca8b29b35354e858318c8b43ee8078c58c879d0b
dongyeobYu <maintain98@naver.com>
2024-06-24 10:21:47 +0900

    create kafkaChat

---
+++ kafkaChat/src/main/java/com/example/kafkachat/KafkaChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaChatApplication.class, args);
+    }
+
+}
---
+++ kafkaChat/src/test/java/com/example/kafkachat/KafkaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
