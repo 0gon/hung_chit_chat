c69b3988138d03541a96536b6592b1551770c3fa
dongyeobYu <maintain98@naver.com>
2024-07-23 08:52:32 +0900

    java -> groovy 로 변경, demo 삭제

--- MemberService/build.gradle.kts
+++ MemberService/build.gradle.kts
@@ -26,9 +26,13 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.apache.groovy:groovy")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	testImplementation("org.springframework.security:spring-security-test")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	runtimeOnly("com.h2database:h2")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
