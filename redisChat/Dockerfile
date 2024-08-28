adc6871a355b1a000711f0fa931560cc2dbb4412
Kim Yeong Gon <dudrhs4563@naver.com>
2024-08-28 17:30:15 +0900

    delete: redisChat 제거

--- redisChat/Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM openjdk:17
-ARG JAR_FILE=*.jar
-COPY target/${JAR_FILE} app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]
--- redisChat/src/main/java/com/redis/redisChat/demo/server/domain/member/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.redis.redisChat.demo.server.domain.member.repository;
-
-import com.redis.redisChat.demo.server.domain.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, String> {
-}
