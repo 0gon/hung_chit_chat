4bfc0e3a06a1272d62539731be8aef9daabab65f
AnYoonKyoung <anky0425@naver.com>
2024-10-03 14:51:52 +0900

    feat(front):프론트 초기설정

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // '/api'로 시작하는 요청을 타겟 서버로 프록시합니다.
+      "/api": {
+        target: "http://localhost:8000", // 백엔드 서버 URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), // 경로에서 /api를 제거합니다.
+      },
+    },
+  },
+});
